278a7b78a479a9775f293543d416b048545212f3
Willy1123 <guillermoblascoperez@gmail.com>
2022-12-11 23:42:40 +0100

    Hola Mundo! del Front

---
+++ src/main/java/com/dis/javalovers/gestionZonasBasicasSalud/GreetService.java
@@ -0,0 +1,12 @@
+package com.dis.javalovers.gestionZonasBasicasSalud;
+
+public class GreetService {
+
+    public String greet(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello anonymous user";
+        } else {
+            return "Hello " + name;
+        }
+    }
+}
